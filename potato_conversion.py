c3f5ea60cc43a8802a8e7d6384a2852cd89b1e0b
Oscar Ramos, Jr. <potatobucket223@gmail.com>
2026-07-02 08:20:14 -0500

    Adding unit conversion to the script.

---
+++ potato_conversion.py
@@ -0,0 +1,11 @@
+def celcius_to_farenheit(celcius: float):
+    """
+Converts temperatures from celcius to real units.
+    """
+    return (celcius * 1.8) + 32
+
+def farenheit_to_celcius(farenheit: float):
+    """
+Converts temperatures from real units to phony-baloney units.
+    """
+    return (farenheit - 32) / 1.8
