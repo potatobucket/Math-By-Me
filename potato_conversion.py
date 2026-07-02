e762eb8b210fb87f2f7af152233bc4eb37088c74
Oscar Ramos, Jr. <potatobucket223@gmail.com>
2026-07-02 08:23:25 -0500

    Misspelled a word and fixed it.

--- potato_conversion.py
+++ potato_conversion.py
@@ -1,10 +1,10 @@
-def celcius_to_farenheit(celcius: float):
+def celcius_to_fahrenheit(celcius: float):
     """
 Converts temperatures from celcius to real units.
     """
     return (celcius * 1.8) + 32
 
-def farenheit_to_celcius(farenheit: float):
+def fahrenheit_to_celcius(farenheit: float):
     """
 Converts temperatures from real units to phony-baloney units.
     """
